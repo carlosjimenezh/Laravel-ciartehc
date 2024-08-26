41b51123a003ef76faff437a0c341232c623307f
carlosejimenezh95 <carlosejimenezh95@gmail.com>
2024-08-25 21:07:14 -0600

    linked team items

--- resources/views/equipo.blade.php
+++ resources/views/equipo.blade.php
@@ -2,7 +2,7 @@
     <section class="my-[250px] mb-[350px] p-4 relative container">
         <h1 class="text-3xl md:text-5xl mb-10 font-bold text-center">Equipo CIARTEHC</h1>
         <div class="grid mb-8 border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 md:mb-12 md:grid-cols-2 bg-white dark:bg-gray-800">
-            <figure class="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-ss-lg md:border-e dark:bg-gray-800 dark:border-gray-700">
+            <figure id="jorge" class="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-ss-lg md:border-e dark:bg-gray-800 dark:border-gray-700">
                 <blockquote class="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
                     <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Very easy this was to integrate</h3>
                     <p class="my-4">If you care for your time, I hands down would go with this."</p>
@@ -18,7 +18,7 @@
                     Descargar CV
                 </button>
             </figure>
-            <figure class="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 md:rounded-se-lg dark:bg-gray-800 dark:border-gray-700">
+            <figure id="rosa" class="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 md:rounded-se-lg dark:bg-gray-800 dark:border-gray-700">
                 <blockquote class="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
                     <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Solid foundation for any project</h3>
                     <p class="my-4">Designing with Figma components that can be easily translated to the utility classes of Tailwind CSS is a huge timesaver!"</p>
@@ -34,7 +34,7 @@
                     Descargar CV
                 </button>
             </figure>
-            <figure class="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 md:rounded-es-lg md:border-b-0 md:border-e dark:bg-gray-800 dark:border-gray-700">
+            <figure id="edith" class="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 md:rounded-es-lg md:border-b-0 md:border-e dark:bg-gray-800 dark:border-gray-700">
                 <blockquote class="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
                     <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Mindblowing workflow</h3>
                     <p class="my-4">Aesthetically, the well designed components are beautiful and will undoubtedly level up your next application."</p>
@@ -50,7 +50,7 @@
                     Descargar CV
                 </button>
             </figure>
-            <figure class="flex flex-col items-center justify-center p-8 text-center bg-white border-gray-200 rounded-b-lg md:rounded-se-lg dark:bg-gray-800 dark:border-gray-700">
+            <figure id="alonso" class="flex flex-col items-center justify-center p-8 text-center bg-white border-gray-200 rounded-b-lg md:rounded-se-lg dark:bg-gray-800 dark:border-gray-700">
                 <blockquote class="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
                     <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Efficient Collaborating</h3>
                     <p class="my-4">You have many examples that can be used to create a fast prototype for your team."</p>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -119,26 +119,26 @@
         <h1 class="text-3xl md:text-5xl mb-10 font-bold">Equipo</h1>
         <!-- usuario -->
         <div class="grid grid-cols-1 md:grid-cols-4 gap-4">
-            <article data-id={id} class="p-2 transition-transform cursor-pointer">
+            <a href="/equipo#jorge" class="p-2 transition-transform">
                 <img class="inline-block h-[250px] w-[250px] rounded-full object-contain relative bg-[#8f993e]" src="images/equipo/jorge.png" alt="">
                 <p class="p-2 font-bold leading-tight text-sm">Dr. Jorge Arturo Martínez Ibarra</p>
                 <p class="p-2 text-sm">Director CIARTEHC</p>
-            </article>
-            <article data-id={id} class="p-2 transition-transform cursor-pointer">
+            </a>
+            <a href="/equipo#rosa" class="p-2 transition-transform">
                 <img class="inline-block h-[250px] w-[250px] rounded-full object-contain relative bg-[#c2ded1]" src="images/equipo/rosa.png" alt="">
                 <p class="p-2 font-bold leading-tight text-sm">Dra. Rosa Elena Arellano Montoya</p>
                 <p class="p-2 text-sm">Profesora titular CIARTEHC</p>
-            </article>
-            <article data-id={id} class="p-2 transition-transform cursor-pointer">
+            </a>
+            <a href="/equipo#edith" class="p-2 transition-transform">
                 <img class="inline-block h-[250px] w-[250px] rounded-full object-contain relative bg-[#b12028]" src="images/equipo/edith.png" alt="">
                 <p class="p-2 font-bold leading-tight text-sm">Lic. Edith Rosario Covarrubias Rodríguez</p>
                 <p class="p-2 text-sm">Técnica académica CIARTEHC</p>
-            </article>
-            <article data-id={id} class="p-2 transition-transform cursor-pointer">
+            </a>
+            <a href="/equipo#alonso" class="p-2 transition-transform">
                 <img class="inline-block h-[250px] w-[250px] rounded-full object-contain relative bg-[#202945]" src="images/equipo/alonso.png" alt="">
                 <p class="p-2 font-bold leading-tight text-sm">Lic. Alonso Sánchez Fabián</p>
                 <p class="p-2 text-sm">Técnico académico CIARTEHC</p>
-            </article>
+            </a>
         </div>
     </section>
 
